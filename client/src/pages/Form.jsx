71cac1faf8539f008d982cd531b0c145efa6a6df
Genc98 <gencgashi98@gmail.com>
2025-02-10 17:33:33 +0100

    Added footer

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -4,22 +4,30 @@ function Form(){
     <header>
       <div className="Logo">
           <img class="logoPicture" src="https://img.freepik.com/free-vector/chatbot-chat-message-vectorart_78370-4104.jpg?t=st=1739194345~exp=1739197945~hmac=c1020f36982eb3b68289d6519c9cfe6eef14dd88a25eadaf2b45cf453eea3d25&w=740"/>
-          <h1>CRM System</h1>
       </div>
+      <h1>CRM System</h1>
       <div class="sidebutton">
-             <button class="loginBtn">Login</button>
+             <button class="loginBtn">Log in</button>
       </div>
     </header>
-    
-      <div className="form">
+
+    <main>
+       <div className="form">
          <h2>Contact Form</h2>
          <input type="text" placeholder="Email"/>
          <input type="text" placeholder="Product"></input>
          <textarea placeholder="Message" rows = "6"></textarea>
          <button className="send">Send</button>
          
-      </div>
-      </>
+       </div>
+    </main>
+
+    <footer>
+      <p>CRM System</p>
+      <p>+46867849305</p>
+      <p>Crm_G5@gmail.com</p>
+    </footer>
+    </>
   )
 }
 export default Form
